1528a1538da8f4d3340cb404ea667991fc373ff4
Henry Richardson <hrichardson83@gmail.com>
2019-02-04 22:34:03 -0500

    edited components/Link.js for final code

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,20 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Link = ({ active, children, onClick}) => {
-  if (active) {
-    return <span>{children}</span>
-  }
   return (
     // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <a
-      href=""
-      onClick={e => {
-        e.preventDefault()
-        onClick()
+    <button
+      onClick={onClick}
+      disabled={active}
+      style={{
+        marginLeft: '4px'
       }}
     >
       {children}
-    </a>
+    </button>
   );
 };
 
