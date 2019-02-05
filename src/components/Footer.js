cfc3888740e028f0b9c612c5fd9a28fdeeaae68e
Henry Richardson <hrichardson83@gmail.com>
2019-02-04 22:36:08 -0500

    edited components/footer.js for final code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,12 @@ import { VisibilityFilters } from '../actions'
 
 const Footer = () => {
   return (
-    <p>
-      Show: <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
-      {', '}
+    <div>
+      <span>Show: </span> 
+      <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
       <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
-      {', '}
       <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
-    </p>
+    </div>
   );
 };
 
